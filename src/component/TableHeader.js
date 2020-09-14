1bbfee3a102a863ac9553654c30947028b3923fa
Sazzad Hossain <sazzad.cse14@gmail.com>
2020-09-14 20:38:11 +0600

    Components

---
+++ src/component/TableHeader.js
@@ -0,0 +1,11 @@
+export default function tableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Book</th>
+                <th>Author(s)</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+    );
+};
